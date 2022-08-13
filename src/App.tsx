043cd3fe75764835bbc63419215643ebb0e83cb7
dipu-sah <111067783+dipu-sah@users.noreply.github.com>
2022-08-13 13:00:40 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div>
+      <p className={"font-bold"}>Hello CodeSandbox</p>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
